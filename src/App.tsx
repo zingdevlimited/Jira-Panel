c40e4905c5761c322b4293276338c35e4e4b2c72
Marion Laanemae <ml553@sussex.ac.uk>
2019-10-01 08:58:30 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const globalWindow:any = global;
+
 const App: React.FC = () => {
+  const [key, setkey] = useState();
+
+  useEffect(() =>{
+    globalWindow.window.AP.context.getContext(function(response:any){
+      console.log(response);
+      setkey(response.jira.issue.key);
+    })
+  })
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {key}
     </div>
   );
 }
